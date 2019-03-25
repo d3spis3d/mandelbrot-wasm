42cca0ee5266f4675be05002073ed586ef578a8f
Daniel Budden <daniel.l.budden@gmail.com>
2019-03-25 21:17:25 +1100

    Successfully generate and download fractal image

--- www/index.js
+++ www/index.js
@@ -2,4 +2,12 @@ import { Mandelbrot } from "mandelbrot-wasm"
 
 const m = Mandelbrot.new(4000, 3000, "-1.20,0.35", "-1,0.20")
 
-console.log(m.render())
+const file = new Blob([m.render()], {type : 'image/png'});
+
+const link = document.createElement("a")
+
+link.download = "mandelbrot.png"
+link.href = URL.createObjectURL(file)
+link.innerText = "Download Image"
+
+document.body.appendChild(link)
