f5e1d70ffed3a8782a9e2d777e77f7df3be4d458
Daniel Budden <daniel.l.budden@gmail.com>
2019-03-26 15:10:33 +1100

    Add simple form to control generation of mandelbrot set

--- www/index.js
+++ www/index.js
@@ -1,13 +1,43 @@
 import { Mandelbrot } from "mandelbrot-wasm"
 
-const m = Mandelbrot.new(4000, 3000, "-1.20,0.35", "-1,0.20")
+function work(width, height, upperLeft, lowerRight) {
+  const m = Mandelbrot.new(width, height, upperLeft, lowerRight)
 
-const file = new Blob([m.render()], {type : 'image/png'});
+  const file = new File([m.render()], {type : 'image/png'});
 
-const link = document.createElement("a")
+  const link = document.createElement("a")
 
-link.download = "mandelbrot.png"
-link.href = URL.createObjectURL(file)
-link.innerText = "Download Image"
+  link.download = "mandelbrot.png"
+  link.href = URL.createObjectURL(file)
 
-document.body.appendChild(link)
+  link.click()
+
+  const button = document.getElementById("submit")
+  const generating = document.getElementById("generating")
+
+  button.className = ""
+  generating.className = "hidden"
+}
+
+const generateAndDownloadMandelbrot = function(event) {
+  event.preventDefault()
+  const [width, height, upperLeft, lowerRight] = event.target
+
+  const button = document.getElementById("submit")
+  const generating = document.getElementById("generating")
+
+  button.className = "hidden"
+  generating.className = ""
+
+  setTimeout(
+    () => work(
+      parseInt(width.value),
+      parseInt(height.value),
+      upperLeft.value,
+      lowerRight.value
+    )
+  )
+}
+
+const form = document.getElementById("mandelbrot-form")
+form.onsubmit = generateAndDownloadMandelbrot
